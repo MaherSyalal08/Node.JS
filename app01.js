8c05b0487e5ce48f526b161bb72bab19220253aa
MaherSyalal08 <2372058@maranatha.ac.id>
2025-05-10 13:19:10 +0700

    Introduction Node.Js

--- app01.js
+++ app01.js
@@ -1,32 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
-const app = express();
+const server = http.createServer((req, res) => {
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'formMahasiswa.html'));
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    if (pathname.startsWith('/public/')) {
+        const fileLocation = path.join(__dirname, pathname);
+        const extname = path.extname(fileLocation);
+        
+        let contentType = 'text/html';
+        if (extname === '.css') contentType = 'text/css';
+        if (extname === '.js') contentType = 'text/javascript';
+
+        fs.readFile(fileLocation, (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                return res.end("404 Not Found</h1>");
+            }
+            
+            res.writeHead(200, { "Content-Type": contentType });
+            res.write(data);
+            return res.end();
+        });
+
+        return; 
+    }
+
+    let fileLocation;
+    switch (pathname) {
+        case '/':
+        case '/form-mahasiswa':
+            fileLocation = path.join(__dirname, 'views', 'formMahasiswa.html');
+            break;
+        case '/form-dosen':
+            fileLocation = path.join(__dirname, 'views', 'formDosen.html');
+            break;
+        case '/view-mahasiswa':
+            fileLocation = path.join(__dirname, 'views', 'viewMahasiswa.html');
+            break;
+        case '/view-dosen':
+            fileLocation = path.join(__dirname, 'views', 'viewDosen.html');
+            break;
+        default:
+            fileLocation = path.join(__dirname, 'views', '404.html');
+            break;
+    }
+
+    fs.readFile(fileLocation, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            return res.end("404 Not Found");
+        }
+
+        const extname = path.extname(fileLocation);
+        let contentType = 'text/html';
+        if (extname === '.css') contentType = 'text/css';
+        if (extname === '.js') contentType = 'text/javascript';
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.write(data);
+        return res.end();
+    });
 });
 
-app.get('/form-mahasiswa', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'formMahasiswa.html'));
-});
-
-app.get('/form-dosen', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'formDosen.html'));
-});
-
-app.get('/view-mahasiswa', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'viewMahasiswa.html'));
-});
-
-app.get('/view-dosen', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'viewDosen.html'));
-});
-
-app.listen(8000, () => {
-  console.log(`Server is running at http://localhost:8000`);
+server.listen(8000, () => {
+    console.log("Server running at http://localhost:8000");
 });
